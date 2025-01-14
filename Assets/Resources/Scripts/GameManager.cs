8304f5d1e2fd9bc485925059977c57d2766fd938
ValeriBalan <165626222+ValeriBalan@users.noreply.github.com>
2025-01-14 16:15:07 +0200

    change variables from drawcards to gameManager

--- Assets/Resources/Scripts/GameManager.cs
+++ Assets/Resources/Scripts/GameManager.cs
@@ -1,18 +1,87 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
+    #region Variables
+
+    //public static bool PlayerAnnounceLie;
+    //public static bool EnemyAnnounceLie;
+    public static bool AnnounceLie;
+    public bool isPlayerTurn = true;
+
+    public GameObject Card1;
+    public GameObject Card2;
+    public GameObject PlayerArea;
+    public GameObject EnemyArea;
+    public GameObject RealEnemyCardArea;
+    public GameObject Dropzone;
+
+    public Deck deck;
+
+    public List<GameObject> cards = new List<GameObject>();
+
+    public  static GameManager instance { get; private set; }
+
+
+    #endregion
+
+    #region MonoBehaviour
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    #endregion
+
+    #region Logic
+
+
+
+    /*public void checkchosencard(Card card)// בודק בהכרזה אם הקלף ששמת שונה ממה שאמרת.
     {
-        
+        if ((card.cardNumber != TempCardValue) || (card.cardNumber != TempCardValue - 1) || card.cardNumber != TempCardValue + 1) // בודק אם הקלף ששמת הוא ולידי
+        {
+            AnnounceLie = true;
+        }
+        else
+        {
+            AnnounceLie = false;
+        }
+
+        if (isPlayerTurn)
+        {
+            PlayerAnnounceLie = AnnounceLie;
+            Debug.Log(PlayerAnnounceLie + " (true = your Openning card was a lie , false =  Openning card wasn't a lie)");
+        }
+
+        else
+        {
+            EnemyAnnounceLie = AnnounceLie;
+        }
+    }*/
+
+    public void BTN_Lair()
+    {
+        if(isPlayerTurn)
+        {
+            if (AnnounceLie)
+            {
+                //הבוט שיקר צריך להזיז את הקלפים אל היד של הבוט 
+            }
+        }
+        else
+        {
+            if (AnnounceLie)
+            {
+                //השחקן שיקר צריך להזיז את הקלפים אל היד של השחקן
+            }
+        }
+
     }
+
+    #endregion
 }
