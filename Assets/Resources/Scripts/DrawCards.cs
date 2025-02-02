61be97c984e29c7c428a284b3c170f960fe6eb81
Linoybar123 <Linoybar15@gmail.com>
2025-02-02 22:50:59 +0200

    make the bot learning and fixing draw card

--- Assets/Resources/Scripts/DrawCards.cs
+++ Assets/Resources/Scripts/DrawCards.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-// using System.Security.Permissions;
 using UnityEngine;
 using UnityEngine.UI;
-//using static GameManager;
 
 public class DrawCards : MonoBehaviour
 {
@@ -16,59 +14,81 @@ public class DrawCards : MonoBehaviour
     {
         initGame();
     }
+
     void Awake()
     {
         instance = this;
     }
-
     #endregion
 
     #region Logic
-
     public void OnClick()
     {
-        Card newCard = GameManager.instance.deck.drawCard();
-        if(GameManager.instance.isPlayerTurn)
+        try
         {
-            newCard.transform.SetParent(GameManager.instance.PlayerArea.transform, false);
-            GameManager.instance.isPlayerTurn = false;
-            liarsLuckBot.Instance.BotMoves(GameManager.instance.chosenNumber);
+            Card newCard = GameManager.instance.deck.drawCard();
+            if (newCard == null)
+                return;
+
+            if (GameManager.instance.isPlayerTurn)
+            {
+                newCard.transform.SetParent(GameManager.instance.PlayerArea.transform, false);
+                GameManager.instance.isPlayerTurn = false;
+                liarsLuckBot.Instance.BotMoves(GameManager.instance.chosenNumber);
+            }
+            else
+            {
+                newCard.transform.SetParent(GameManager.instance.RealEnemyCardArea.transform, false);
+                GameManager.instance.isPlayerTurn = true;
+                liarsLuckBot.Instance._unityButtonLair["Button_Cheat"].GetComponent<Button>().interactable = true;
+                GameManager.instance.SyncEnemyArea();
+            }
         }
-        else
+        catch (System.Exception e)
         {
-            newCard.transform.SetParent(GameManager.instance.RealEnemyCardArea.transform, false);
-            GameManager.instance.isPlayerTurn = true;
-            liarsLuckBot.Instance._unityButtonLair["Button_Cheat"].GetComponent<Button>().interactable = true;
+            Debug.LogError($"Error in OnClick: {e.Message}");
         }
     }
 
     public void initGame()
     {
-        GameManager.instance.deck.createDeck();
-        for (var i = 0; i < 10; i++)
+        try
         {
-            Card playerCard = GameManager.instance.deck.drawCard();
-            playerCard.transform.SetParent(GameManager.instance.PlayerArea.transform, false);
+            GameManager.instance.deck.createDeck();
+            for (var i = 0; i < 10; i++)
+            {
+                Card playerCard = GameManager.instance.deck.drawCard();
+                if (playerCard != null)
+                {
+                    playerCard.transform.SetParent(GameManager.instance.PlayerArea.transform, false);
+                }
 
-            GameObject enemyCard = Instantiate(GameManager.instance.Card2, new Vector3(0, 0, 0), Quaternion.identity);
-            enemyCard.transform.SetParent(GameManager.instance.EnemyArea.transform, false);
+                GameObject enemyCard = Instantiate(GameManager.instance.Card2, new Vector3(0, 0, 0), Quaternion.identity);
+                if (enemyCard != null)
+                {
+                    enemyCard.transform.SetParent(GameManager.instance.EnemyArea.transform, false);
+                }
 
-            Card realEnemyCard = GameManager.instance.deck.drawCard();
-            realEnemyCard.transform.SetParent(GameManager.instance.RealEnemyCardArea.transform, false);
+                Card realEnemyCard = GameManager.instance.deck.drawCard();
+                if (realEnemyCard != null)
+                {
+                    realEnemyCard.transform.SetParent(GameManager.instance.RealEnemyCardArea.transform, false);
+                }
+            }
 
+            Card firstCard = GameManager.instance.deck.drawCard();
+            if (firstCard != null)
+            {
+                firstCard.transform.SetParent(GameManager.instance.Dropzone.transform, false);
+                Card firstCardDup = Instantiate(firstCard, GameManager.instance.DropZoneStack.transform, false);
+            }
+
+            liarsLuckBot.Instance.InitializeBot();
         }
-        Card firstCard = GameManager.instance.deck.drawCard();
-        firstCard.transform.SetParent(GameManager.instance.Dropzone.transform, false);
-        Card firstCardDup = Instantiate(firstCard, GameManager.instance.DropZoneStack.transform, false); 
-        liarsLuckBot.Instance.InitializeBot();
+        catch (System.Exception e)
+        {
+            Debug.LogError($"Error in initGame: {e.Message}");
+        }
     }
-
-    //private Card GetCard(GameObject currentArea)
-    //{
-    //    Card drawnCard = deck.drawCard();
-    //    drawnCard.transform.SetParent(currentArea.transform, false);
-    //    return drawnCard;
-    //}
-
     #endregion
 }
